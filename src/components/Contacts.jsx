b5dd55576fd087446c2704b621785c2e0df8e400
aripbudiman <muhamadarifbudiman22@gmail.com>
2024-01-03 05:49:42 +0700

    membuat store message

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,7 +1,74 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Icon } from '@iconify/react';
 
 export default function Contacts() {
+    const [error, setError] = useState({
+        fullname: "",
+        email: "",
+        subject: "",
+        message: ""
+    });
+    const [messages, setMessages] = useState({
+        fullname: "",
+        email: "",
+        subject: "",
+        message: "",
+        created_at: ""
+    });
+
+    const storeToFirebase = () => {
+        if (messages.fullname === "" || messages.email === "" || messages.subject === "" || messages.message === "") {
+            setError({
+                fullname: messages.fullname === "" ? "Fullname is required" : "",
+                email: messages.email === "" ? "Email is required" : "",
+                subject: messages.subject === "" ? "Subject is required" : "",
+                message: messages.message === "" ? "Message is required" : ""
+            })
+            return;
+        }
+        fetch('https://aripbudiman-default-rtdb.firebaseio.com/contacts.json', {
+            method: 'POST',
+            body: JSON.stringify(messages),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(() => {
+            setMessages({
+                fullname: "",
+                email: "",
+                subject: "",
+                message: "",
+            });
+            setError({
+                fullname: "",
+                email: "",
+                subject: "",
+                message: ""
+            })
+        });
+    }
+    const change = (e) => {
+        setMessages({
+            ...messages,
+            [e.target.name]: e.target.value
+        })
+    }
+    useEffect(() => {
+        const dateTime = () => {
+            const timestamp = Date.now();
+            const date = new Date(timestamp);
+            const day = String(date.getDate()).padStart(2, '0');
+            const month = String(date.getMonth() + 1).padStart(2, '0');
+            const year = date.getFullYear();
+            const hours = String(date.getHours()).padStart(2, '0');
+            const minutes = String(date.getMinutes()).padStart(2, '0');
+            const seconds = String(date.getSeconds()).padStart(2, '0');
+            const formattedDate = `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+            setMessages({ ...messages, created_at: formattedDate });
+        }
+        dateTime();
+    }, [])
+    console.log(error);
     return (
         <div id='contacts' className='bg-base-100'>
             <h1 className='text-center text-4xl font-semibold py-10'>Contacts</h1>
@@ -13,28 +80,32 @@ export default function Contacts() {
                             <div className="label">
                                 <span className="label-text font-semibold">FULL NAME</span>
                             </div>
-                            <input type="text" placeholder="Full name" className="input input-bordered w-full max-w-xs" />
+                            <input name='fullname' value={messages.fullname} onChange={change} type="text" placeholder="Full name" className={"input input-bordered w-full max-w-xs " + (error.fullname ? "input-error" : "")} />
+                            <p className='text-error'>{error.fullname}</p>
                         </label>
                         <label className="form-control w-full max-w-xs">
                             <div className="label">
                                 <span className="label-text font-semibold">EMAIL</span>
                             </div>
-                            <input type="text" placeholder="Email" className="input input-bordered w-full max-w-xs" />
+                            <input name='email' value={messages.email} onChange={change} type="text" placeholder="Email" className={"input input-bordered w-full max-w-xs " + (error.email ? "input-error" : "")} />
+                            <p className='text-error'>{error.email}</p>
                         </label>
                     </div>
                     <label className="form-control w-full">
                         <div className="label">
                             <span className="label-text font-semibold">SUBJECT</span>
                         </div>
-                        <input type="text" placeholder="Subject" className="input input-bordered w-full" />
+                        <input name='subject' value={messages.subject} onChange={change} type="text" placeholder="Subject" className={"input input-bordered w-full " + (error.subject ? "input-error" : "")} />
+                        <p className='text-error'>{error.subject}</p>
                     </label>
                     <label className="form-control">
                         <div className="label">
                             <span className="label-text font-semibold">MESSAGE</span>
                         </div>
-                        <textarea className="textarea textarea-bordered h-32" placeholder="Message"></textarea>
+                        <textarea name='message' value={messages.message} onChange={change} className={"textarea textarea-bordered h-32 " + (error.message ? "textarea-error" : "")} placeholder="Message"></textarea>
+                        <p className='text-error'>{error.message}</p>
                     </label>
-                    <button className='btn btn-primary mt-5 w-1/3'>Send Message</button>
+                    <button onClick={storeToFirebase} className='btn btn-primary mt-5 w-1/3'>Send Message</button>
                 </div>
                 <GoogleMap />
             </div>
